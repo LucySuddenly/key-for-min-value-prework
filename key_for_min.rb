f982c08fc66b8a6acc78b5e75293658892c2dda9
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-16 22:53:03 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,7 +10,7 @@ def key_for_min_value(name_hash)
     name_hash.each do |key, value|
       check = value
       if check > min_key
-        min_key = see?
+        min_key = check
       end 
     end
   end
